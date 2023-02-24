41c904b54cbbda84f326ea6c69bed5e10b37dc21
Vincent <thanhvinhnguyenduc@gmail.com>
2023-02-24 18:24:46 +1100

    Added Triangle class

---
+++ src/main/Triangle.java
@@ -0,0 +1,45 @@
+package main;
+import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Triangle {
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Color color;
+
+    Triangle(Vertex v1, Vertex v2, Vertex v3, Color color) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+
+    public static List<Triangle> generateTriangle() {
+
+        List<Triangle> triangles = new ArrayList<>();
+
+        triangles.add(new Triangle(new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(-100, 100, -100),
+                Color.WHITE));
+
+        triangles.add(new Triangle(new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(100, -100, -100),
+                Color.RED));
+
+        triangles.add(new Triangle(new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(100, 100, 100),
+                Color.GREEN));
+
+        triangles.add(new Triangle(new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(-100, -100, 100),
+                Color.BLUE));
+
+        return triangles;
+    }
+}
