5afddd2f88a0bc2002f0b917dc514bf6aed6a11d
Vincent <thanhvinhnguyenduc@gmail.com>
2023-02-24 18:49:35 +1100

    separated Render class and DemoView class

--- src/main/DemoViewer.java
+++ src/main/DemoViewer.java
@@ -2,16 +2,11 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.List;
-import java.util.ArrayList;
-import java.awt.geom.*;
-import java.awt.image.BufferedImage;
+
 
 public class DemoViewer {
 
-    public static void main(String[] args) {
-        viewDemo();
-    }
+    DemoViewer(){}
 
     public static void viewDemo() {
 
@@ -28,39 +23,10 @@ public class DemoViewer {
         pane.add(pitchSlider, BorderLayout.EAST);
 
         // panel to display render results
-        JPanel renderPanel = new JPanel() {
-            @Override
-            public void paintComponent(Graphics g) {
-                Graphics2D g2 = (Graphics2D) g;
-                g2.setColor(Color.BLACK);
-                g2.fillRect(0, 0, getWidth(), getHeight());
-                g2.translate(getWidth() / 2, getHeight() / 2);
-                g2.setColor(Color.WHITE);
-
-                // Triangles rendering
-                renderTriangles(g2);
-
-            }
-        };
-        pane.add(renderPanel, BorderLayout.CENTER);
+        pane.add(Render.renderPanel(), BorderLayout.CENTER);
 
         frame.setSize(400, 400);
         frame.setVisible(true);
     }
 
-    private static void renderTriangles(Graphics2D g2) {
-        
-
-        List<Triangle> triangles = Triangle.generateTriangle();
-
-        for (Triangle t : triangles) {
-            Path2D path = new Path2D.Double();
-            path.moveTo(t.v1.x, t.v1.y);
-            path.lineTo(t.v2.x, t.v2.y);
-            path.lineTo(t.v3.x, t.v3.y);
-            path.closePath();
-            g2.draw(path);
-        }
-
-    } 
 }
